7f0a6acce5af2f3d7ce99bfacbb43512736bdd4c
Zhengsheng Zhou <zhengshengz@vmware.com>
2020-05-12 20:53:46 +0800

    Add OSSTP Script

---
+++ osstp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -xe
+
+RESULTS=`pwd`/osstp_results
+ANTREA_ROOTDIR=`pwd`/antrea/src
+
+mkdir -p "${RESULTS}"
+
+# https://gitlab.eng.vmware.com/core-build/mirrors_internal_osstptool
+pushd antrea/src
+go mod vendor
+popd
+osstptool generate -o "${RESULTS}/osstp_golang.yml" "--root=${ANTREA_ROOTDIR}" antrea gitlab.eng.vmware.com/core-build/mirrors_github_antrea
+
+pushd "${RESULTS}"
+osstptool download "--root=${ANTREA_ROOTDIR}"
+
+# Need to install virtualenv first
+# https://osm.eng.vmware.com/doc/utilities/access.html
+workon osstp
+# Upload the source code and create master package on OSM site.
+# https://osm.eng.vmware.com/doc/utilities/loading.html
+~/antrea-repos/osstpclients/bin/osstp-load.py -I 'Distributed - Static Link w/ VMW' -U zhengshengz osstp_golang.yml
+deactivate
+popd
