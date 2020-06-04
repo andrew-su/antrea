7d40296c9ca8770caa47ff8fdc5de2c98571bb80
Zhengsheng Zhou <zhengshengz@vmware.com>
2020-06-04 09:53:33 +0800

    Manually Upload OSSTP Result
    OSSTP client needs some python library packages. The user can
    manually setup a virtualenv and upload it.

--- osstp.sh
+++ osstp.sh
@@ -18,9 +18,10 @@ osstptool download "--root=${ANTREA_ROOTDIR}"
 
 # Need to install virtualenv first
 # https://osm.eng.vmware.com/doc/utilities/access.html
-workon osstp
+echo Please do the following steps manually
+echo cd $(pwd) ; workon osstp
 # Upload the source code and create master package on OSM site.
 # https://osm.eng.vmware.com/doc/utilities/loading.html
-~/antrea-repos/osstpclients/bin/osstp-load.py -I 'Distributed - Static Link w/ VMW' -U zhengshengz osstp_golang.yml
-deactivate
+echo ~/antrea-repos/osstpclients/bin/osstp-load.py -I 'Distributed - Static Link w/ VMW' -U zhengshengz osstp_golang.yml
+echo deactivate
 popd
