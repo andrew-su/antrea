0e6dd3f8f070cc05dfe1d052f801b4cf6c003056
Xu Liu <xliu2@vmware.com>
2022-07-20 13:42:24 +0800

    Update Photon build number
    version-change: false
    
    Signed-off-by: Xu Liu <xliu2@vmware.com>

--- support/gobuild/specs/cayman_antrea.py
+++ support/gobuild/specs/cayman_antrea.py
@@ -22,7 +22,7 @@ CAYMAN_OPENSSL_HOSTTYPES = {
 }
 
 CSC_PHOTON_BRANCH = "photon3-vmw-updates"
-CSC_PHOTON_CLN = "9313915"
+CSC_PHOTON_CLN = "10085085"
 CSC_PHOTON_BUILDTYPE = 'release'
 CSC_PHOTON_FILES = {
     LINUX_HOSTTYPE: ["publish/docker-image/photon-rootfs.tar.gz",
