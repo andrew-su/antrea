0cb7dc178685d448b00f717221b4e25fde9c2b90
Xu Liu <xliu2@vmware.com>
2022-07-29 12:23:47 +0800

    Publish multi-cluster UBI image
    version-change: false
    
    Signed-off-by: Xu Liu <xliu2@vmware.com>

--- antrea/antrea_multi-cluster.sh
+++ antrea/antrea_multi-cluster.sh
@@ -14,46 +14,54 @@ compile_e2e "noipsec" "multi-cluster"
 
 echo "====== Preparing Antrea Multi-cluster Deliverables: Manifests ======"
 git status
-antrea_mc_deliverables="antrea-multicluster-${ANTREA_VERSION_DIGIT}"
-mkdir -p "${PUBLISH_DIR}/${antrea_mc_deliverables}"
-mkdir -p "${PUBLISH_DIR}/${antrea_mc_deliverables}/manifests"
-echo "${BUILD_NUMBER}" > "${PUBLISH_DIR}/${antrea_mc_deliverables}/build_number.txt"
+OUTPUT_DIR="${BUILDROOT}/mc-output"
+mkdir -p "${OUTPUT_DIR}/manifests"
+echo "${BUILD_NUMBER}" > "${OUTPUT_DIR}/manifests/build_number.txt"
 for yml_file in ${REPO_ROOT}/multicluster/build/yamls/*.yml; do
   base_name="$(basename $yml_file .yml)"
   sed \
-    -e "s/image: antrea\/antrea-.*\$/image: antrea\/antrea-mc-controller-debian:${IMAGE_VERSION}/g" \
-    -e "s/image: projects.registry.vmware.com\/antrea\/antrea-.*\$/image: antrea\/antrea-mc-controller-debian:${IMAGE_VERSION}/g" \
-    "$yml_file" > "${PUBLISH_DIR}/${antrea_mc_deliverables}/manifests/${base_name}-${BINARY_VERSION}.yml"
+    -e "s/image: antrea\/antrea-.*\$/image: antrea\/antrea-mc-controller:${IMAGE_VERSION}/g" \
+    -e "s/image: projects.registry.vmware.com\/antrea\/antrea-.*\$/image: antrea\/antrea-mc-controller:${IMAGE_VERSION}/g" \
+    "$yml_file" > "${OUTPUT_DIR}/manifests/${base_name}-${BINARY_VERSION}.yml"
 done
-cp -r "${REPO_ROOT}/multicluster/config/samples/clusterset_init" "${PUBLISH_DIR}/${antrea_mc_deliverables}/manifests"
+cp -r "${REPO_ROOT}/multicluster/config/samples/clusterset_init" "${OUTPUT_DIR}/manifests"
 
-echo "====== Building antrea-mc-controller-debian Image ======"
-make antrea-mc-controller
-docker tag antrea/antrea-mc-controller "antrea/antrea-mc-controller-debian:${IMAGE_VERSION}"
+echo "====== Building antrea-mc-controller Binaries ======"
+fips_make "go build -o bin/antrea-mc-controller antrea.io/antrea/multicluster/cmd/..."
 
-echo "====== Saving Antrea Multi-cluster Product Deliverables ======"
-OUTPUT_DIR="${BUILDROOT}/mc-output"
+echo "====== Building antrea-mc-controller Debian Image ======"
+make antrea-mc-controller-debian VERSION=${IMAGE_VERSION}
+
+echo "====== Building antrea-mc-controller UBI Image ======"
+make antrea-mc-controller-ubi VERSION=${IMAGE_VERSION}
 
 echo "====== Saving and Signing Antrea Multi-cluster Product Images ======"
+for variant in "debian" "ubi"; do
+  mkdir -p "${OUTPUT_DIR}/images-${variant}"
+  image_id="$(docker inspect -f '{{.ID}}' "antrea/antrea-mc-controller-${variant}:${IMAGE_VERSION}")"
+  docker tag antrea/antrea-mc-controller-${variant}:${IMAGE_VERSION} antrea/antrea-mc-controller:${IMAGE_VERSION}
+  docker save antrea/antrea-mc-controller-${variant}:${IMAGE_VERSION} antrea/antrea-mc-controller:${IMAGE_VERSION} | gzip -9 > "${OUTPUT_DIR}/images-${variant}/antrea-mc-controller-${IMAGE_VERSION}.tar.gz"
+  digest_filename="antrea-mc-controller-${IMAGE_VERSION}-image-digests.txt"
+  echo "antrea/antrea-mc-controller@${image_id}" > "${OUTPUT_DIR}/images-${variant}/${digest_filename}"
 
-image_id="$(docker inspect -f '{{.ID}}' "antrea/antrea-mc-controller-debian:${IMAGE_VERSION}")"
-digest_filename="antrea-mc-controller-debian-${IMAGE_VERSION}-image-digests.txt"
-checksum_filename="antrea-mc-controller-debian-${IMAGE_VERSION}-image-checksums.txt"
-mkdir -p "${OUTPUT_DIR}/images"
-docker save antrea/antrea-mc-controller-debian:${IMAGE_VERSION} | gzip -9 > "${OUTPUT_DIR}/images/antrea-mc-controller-debian-${IMAGE_VERSION}.tar.gz"
-echo "antrea/antrea-mc-controller-debian@${image_id}" > "${OUTPUT_DIR}/images/${digest_filename}"
-pushd "${OUTPUT_DIR}/images/"
-sha256sum -- * > ${checksum_filename}
-# See other alternative keys in /build/toolchain/noarch/vmware/gpgsign/officialkey/
-gpgsignc textsign -i ${checksum_filename} -o "${checksum_filename}.asc" --hash=sha256 --keyid=001E5CC9
-popd
+  checksum_filename="antrea-mc-controller-${IMAGE_VERSION}-image-checksums.txt"
+  pushd "${OUTPUT_DIR}/images-${variant}/"
+  sha256sum -- * > ${checksum_filename}
+  # See other alternative keys in /build/toolchain/noarch/vmware/gpgsign/officialkey/
+  gpgsignc textsign -i ${checksum_filename} -o "${checksum_filename}.asc" --hash=sha256 --keyid=001E5CC9
+  popd
 
-echo "====== Preparing Antrea Multi-cluster Deliverables: Images ======"
-cp -r "${OUTPUT_DIR}/images" "${PUBLISH_DIR}/${antrea_mc_deliverables}"
-pushd "${PUBLISH_DIR}"
-zip --verbose -r "${antrea_mc_deliverables}.zip" "${antrea_mc_deliverables}"
-rm -rf "${PUBLISH_DIR}/${antrea_mc_deliverables}" "${OUTPUT_DIR}"
-popd
+  antrea_mc_deliverables="antrea-multicluster-${variant}-${ANTREA_VERSION_DIGIT}"
+  mkdir -p "${PUBLISH_DIR}/${antrea_mc_deliverables}"
+  cp -r "${OUTPUT_DIR}/images-${variant}" "${PUBLISH_DIR}/${antrea_mc_deliverables}/images"
+  cp -r "${OUTPUT_DIR}/manifests" "${PUBLISH_DIR}/${antrea_mc_deliverables}"
+  pushd "${PUBLISH_DIR}"
+  zip --verbose -r "${antrea_mc_deliverables}.zip" "${antrea_mc_deliverables}"
+  rm -rf "${PUBLISH_DIR}/${antrea_mc_deliverables}"
+  rm -rf "${OUTPUT_DIR}/images-${variant}"
+  popd
+done
 
 echo "====== Cleanup Antrea Multi-cluster Product Build Result ======"
+rm -rf "${OUTPUT_DIR}"
 make clean
