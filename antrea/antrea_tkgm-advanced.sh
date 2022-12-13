e73c2bbc7c35a114544bb42329a81686f1172b80
Zhengsheng Zhou <zhengshengz@vmware.com>
2022-12-13 14:41:40 +0800

    Remove IPSec Image and Manifest from TKGm Target
    TKGm doesn't actually use the IPSec image and manifest.

--- antrea/antrea_tkgm-advanced.sh
+++ antrea/antrea_tkgm-advanced.sh
@@ -14,7 +14,6 @@ check_manifests
 
 echo "===== Compile antrea e2e testcases ======"
 compile_e2e "noipsec" "tkgm"
-compile_e2e "ipsec" "tkgm"
 git status
 
 echo "====== Building Binaries for TKGm advanced ======"
@@ -25,8 +24,6 @@ pushd build/images/ovs
 cp ${OVS_DIR}/openvswitch-${OVS_VER}.tar.gz .
 echo "====== Building openvswitch-debian Image ======"
 docker build -f Dockerfile.debian --build-arg OVS_VERSION=${OVS_VER} -t antrea/openvswitch-debian:standard .
-echo "====== Building openvswitch-debian-ipsec Image ======"
-docker build -f Dockerfile.debian --build-arg IPSEC=true --build-arg OVS_VERSION=${OVS_VER} -t antrea/openvswitch-debian:ipsec .
 popd
 
 cp ${GOBUILD_CAYMAN_CNI_PLUGINS_ROOT}/lin64/cni_plugins/executables/cni-plugins-*.tgz .
@@ -37,9 +34,6 @@ make debian VERSION=${IMAGE_VERSION}
 echo "====== Building flow-aggregator-debian Image ======"
 make flow-aggregator-image-debian VERSION=${IMAGE_VERSION}
 
-echo "====== Building antrea-debian-ipsec Image ======"
-make debian-ipsec VERSION=${IMAGE_VERSION}
-
 # Create archives for scripts and binaries
 echo "====== Saving TKGm Deliverables ======"
 # "${BUILDROOT}/output" will be published to lin64/antrea by antrea_defs.py:CaymanAntreaBuilderLin.install
@@ -49,7 +43,6 @@ echo "====== Saving TKGm Manifests ======"
 mkdir -p "${OUTPUT_DIR}/manifests"
 
 # Antrea yamls for TKG
-# antrea-ipsec is not used in TKGm.
 MANIFESTS_DIR=$(mktemp -d)
 IMG_NAME=antrea/antrea-advanced-debian IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hack/generate-standard-manifests.sh --mode release --out "${MANIFESTS_DIR}"
 IMG_NAME=antrea/flow-aggregator-debian IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hack/generate-manifest-flow-aggregator.sh --mode release > "${MANIFESTS_DIR}/flow-aggregator.yml"
@@ -59,10 +52,6 @@ cp ${MANIFESTS_DIR}/antrea-advanced-fips.yml "${OUTPUT_DIR}/manifests/antrea-fip
 cp ${MANIFESTS_DIR}/flow-aggregator.yml "${OUTPUT_DIR}/manifests/flow-aggregator-${BINARY_VERSION}.yml"
 cp ${MANIFESTS_DIR}/antrea-flow-visibility-test.yml "${OUTPUT_DIR}/manifests/antrea-flow-visibility-test-${BINARY_VERSION}.yml"
 
-MANIFESTS_DIR=$(mktemp -d)
-IMG_NAME=antrea/antrea-advanced-debian-ipsec IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hack/generate-standard-manifests.sh --mode release --out "${MANIFESTS_DIR}"
-cp ${MANIFESTS_DIR}/antrea-advanced-ipsec.yml "${OUTPUT_DIR}/manifests/antrea-ipsec-${BINARY_VERSION}.yml"
-
 echo "====== Saving and Signing TKGm Images ======"
 # Image for TKG
 mkdir -p "${OUTPUT_DIR}/images"
@@ -70,8 +59,6 @@ mkdir -p "${OUTPUT_DIR}/images"
 # Just publish Antrea images.
 docker tag antrea/antrea-debian:${IMAGE_VERSION} antrea/antrea-advanced-debian:${IMAGE_VERSION}
 docker save antrea/antrea-advanced-debian:${IMAGE_VERSION} | gzip -9 > "${OUTPUT_DIR}/images/antrea-advanced-debian-${IMAGE_VERSION}.tar.gz"
-docker tag antrea/antrea-debian-ipsec:${IMAGE_VERSION} antrea/antrea-advanced-debian-ipsec:${IMAGE_VERSION}
-docker save antrea/antrea-advanced-debian-ipsec:${IMAGE_VERSION} | gzip -9 > "${OUTPUT_DIR}/images/antrea-advanced-debian-ipsec-${IMAGE_VERSION}.tar.gz"
 docker save antrea/flow-aggregator-debian:${IMAGE_VERSION} | gzip -9 > "${OUTPUT_DIR}/images/flow-aggregator-debian-${IMAGE_VERSION}.tar.gz"
 image_id="$(docker inspect -f '{{.ID}}' "antrea/antrea-advanced-debian:${IMAGE_VERSION}")"
 digest_filename="antrea-advanced-debian-${IMAGE_VERSION}-image-digests.txt"
@@ -88,7 +75,6 @@ rm -rf "${OUTPUT_DIR}/executables"
 mkdir -p "${OUTPUT_DIR}/executables"
 cat "${REPO_ROOT}/bin/antctl" | gzip -9 > "${OUTPUT_DIR}/executables/antctl-${BINARY_VERSION}.gz"
 gzip -c "bin/e2e-tkgm-${ANTREA_VERSION}" > "${OUTPUT_DIR}/executables/e2e-tkgm-${ANTREA_VERSION}.gz"
-gzip -c "bin/e2e-tkgm-ipsec-${ANTREA_VERSION}" > "${OUTPUT_DIR}/executables/e2e-tkgm-ipsec-${ANTREA_VERSION}.gz"
 pushd "${OUTPUT_DIR}/executables"
 BINARY_CHECKSUM_FILENAME="antctl-${BINARY_VERSION}-checksums.txt"
 sha256sum -- * > ${BINARY_CHECKSUM_FILENAME}
