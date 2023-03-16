37db01a061e8bfba6edd8f8414942f0aa3f969dc
Naman Agarwal <naman.agarwal75@gmail.com>
2023-03-16 11:37:22 +0530

    Remove -debian from cayman_antrea_tkgm-standard yaml

--- antrea/antrea_tkgm-standard.sh
+++ antrea/antrea_tkgm-standard.sh
@@ -44,7 +44,7 @@ IMG_NAME=antrea/antrea-standard-debian IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hac
 IMG_NAME=antrea/flow-aggregator-debian IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hack/generate-manifest-flow-aggregator.sh --mode release > "${MANIFESTS_DIR}"/flow-aggregator.yml
 IMG_NAME=antrea/antrea-standard-debian IMG_TAG=${IMAGE_VERSION} ${REPO_ROOT}/hack/generate-manifest.sh --feature-gates FlowExporter=true --extra-helm-values-file "${REPO_ROOT}/ci/kind/values-flow-exporter.yml" --mode release > "${MANIFESTS_DIR}"/antrea-flow-visibility-test.yml
 cp "${MANIFESTS_DIR}/antrea-standard.yml" "${OUTPUT_DIR}/manifests/antrea-standard-${BINARY_VERSION}.yml"
-cp "${MANIFESTS_DIR}/flow-aggregator.yml" "${OUTPUT_DIR}/manifests/flow-aggregator-debian-${BINARY_VERSION}.yml"
+cp "${MANIFESTS_DIR}/flow-aggregator.yml" "${OUTPUT_DIR}/manifests/flow-aggregator-${BINARY_VERSION}.yml"
 cp "${MANIFESTS_DIR}/antrea-flow-visibility-test.yml" "${OUTPUT_DIR}/manifests/antrea-flow-visibility-test-${BINARY_VERSION}.yml"
 
 echo "====== Saving and Signing TKGm Images ======"
