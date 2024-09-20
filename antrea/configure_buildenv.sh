e395d015d5f6fe4da981a8d2da97a0cb386701c3
Lan Luo <luola@vmware.com>
2024-09-20 13:45:20 +0800

    Add required steps back for cayman_antrea_pacakge target

--- antrea/configure_buildenv.sh
+++ antrea/configure_buildenv.sh
@@ -45,3 +45,10 @@ sudo /usr/bin/chown mts /var/run/docker.sock
 sudo /usr/sbin/iptables -I FORWARD -j ACCEPT
 sudo /usr/sbin/ip link set docker0 promisc on
 sudo /usr/bin/systemctl status docker.service
+
+# This step is required on firewalled machines, since host to container traffic is blocked by default
+# it's only for the target cayman_antrea_package, please make sure you trigger a sandbox build
+# for it if there is any change for the following steps.
+echo "Enabling firewall rule to allow traffic from host to docker bridge network"
+bridge_net=$(sudo /usr/bin/docker network inspect --format '{{range .IPAM.Config}}{{.Subnet}}{{end}}' bridge)
+sudo /usr/sbin/iptables -A BUILD_OUT -d ${bridge_net} -m comment --comment "allow traffic from host to docker bridge network" -j ACCEPT || true
