2effc001e9382eebbd2b1aac76828f74c607ad35
CBot (on behalf of xs032484) <xunzhi.sun@broadcom.com>
2024-12-13 13:45:25 -0800

    CBOT: Automatic update of csc-photon/photon5-vmw-updates CLN from 14731972 to 14734731 for cayman_antrea_package on branch vmware-2.2.0+vmware.1.
    gobuild automation - update to component build number
    
    CBot config file: /build/apps/cbot/config/vmware-2.2.0+vmware.1/cayman_antrea_package/csc-photon.yaml
    
    This change was made on behalf of xs032484 by the gobuild
    component robot (aka CBot); see https://wiki.eng.vmware.com/Build/CBot.
    It is bumping up the CLN to the latest build of this component
    which meets the current policy of this branch.
    
    If you are experiencing a build or run-time failure as a result of this
    change, backing it out will not solve the problem as the CBot will
    re-submit the same change again on its next visit to this branch.  In
    addition to backing out the change, you'll want to temporarily
    deactivate the CBot in this branch until the problem with the component
    (or the consumer) is resolved.  See
    https://wiki.eng.vmware.com/Build/CBot#DisableCBot
    
    If you need assistance, contact cayman@vmware.com or xs032484@vmware.com.
    
    Automation: cbot
    Bug Number: none
    QA Notes: Component csc-photon/'photon5-vmw-updates' build at CLN 14734731 has passed.
    Reviewed by: auto
    SVS opt-out rationale: CBot submission
    Testing Done: None. Bump up csc-photon component version.

--- support/gobuild/specs/cayman_antrea.py
+++ support/gobuild/specs/cayman_antrea.py
@@ -22,7 +22,7 @@ CAYMAN_OPENSSL_HOSTTYPES = {
 }
 
 CSC_PHOTON_BRANCH = "photon5-vmw-updates"
-CSC_PHOTON_CLN = 14731972
+CSC_PHOTON_CLN = 14734731
 CSC_PHOTON_BUILDTYPE = 'release'
 CSC_PHOTON_FILES = {
     LINUX_HOSTTYPE: ["publish/docker-image/photon-rootfs.tar.gz",
